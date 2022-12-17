9df4fb7eccb116cbea35501a9f6f87c30e9e7609
SHO MATOBA <2004240018ms@edu.oca.ac.jp>
2022-12-17 15:57:07 +0900

    uu

---
+++ bbbb.py
@@ -0,0 +1,9 @@
+def print_string(param_str, column_count, row_count):
+    """この関数は5文字以上10文字以下でなければエラーメッセージを表示させ、アルファベット(a～z, A～Z)以外が使われていればエラーメッセージを表示させる。
+
+
+    Args:
+        param_str (_type_): 文字列
+        column_count (_type_): 列を繰り返す
+        row_count (_type_): 行を繰り返す
+    """
